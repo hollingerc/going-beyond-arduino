1d0f1292a9d709c896254323d4ffee54f4ed7f8e
Craig Hollinger <craigh1958@gmail.com>
2016-09-10 09:09:40 -0700

    Update blink3.c

--- blink3.c
+++ blink3.c
@@ -9,8 +9,8 @@
  * PORTD4.
  *
  * This file is free software; you can redistribute it and/or modify
- * it under the terms of either the GNU General Public License version 2
- * or the GNU Lesser General Public License version 2.1, both as
+ * it under the terms of either the GNU General Public License version 3
+ * or the GNU Lesser General Public License version 3, both as
  * published by the Free Software Foundation.
 */ 
 
