463e9c71341baaae88a6df40dc3e1e4d691449e8
Craig Hollinger <craigh1958@gmail.com>
2016-09-10 09:08:38 -0700

    Update BlinkWithTC0.c

--- BlinkWithTC0.c
+++ BlinkWithTC0.c
@@ -12,8 +12,8 @@
  * - implement a state machine to keep track of which LED to turn on
  *
  * This file is free software; you can redistribute it and/or modify
- * it under the terms of either the GNU General Public License version 2
- * or the GNU Lesser General Public License version 2.1, both as
+ * it under the terms of either the GNU General Public License version 3
+ * or the GNU Lesser General Public License version 3, both as
  * published by the Free Software Foundation.
  */
 
