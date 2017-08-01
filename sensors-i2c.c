39e5031de1507b3451e56a20c49fc5a6547248b8
Craig Hollinger <craigh1958@gmail.com>
2017-08-01 16:32:03 -0700

    Update sensors-i2c.c
    Corrected an error in the formatting of the magnetometer data.

--- sensors-i2c.c
+++ sensors-i2c.c
@@ -1,33 +1,39 @@
 /*
- * sensors-i2c.c
+ * Sensors.c
  *
- * Created: 2017-07-22
+ * Created: 2017-07-31
  * Author : Craig Hollinger
  *
  * This is an example program that uses the I2C bus to read data from sensors
- * and display it on an OLED display.  The sensors are an accelerometer,
+ * and display it on an SPI OLED display.  The sensors are an accelerometer,
  * a gyroscope and a magnetometer.  Each sensor has three axes of data and 
- * communicates on the I2C bus.  The OLED display also communicates on the I2C
- * bus.
+ * communicates on the I2C bus.  The OLED display communicates on the SPI bus.
  *
  * The I2C lines are connected as follows:
  *    SCL - Arduino A5 (ATMEGA PORTC5)
  *    SDA - Arduino A4 (ATMEGA PORTC4)
  *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of either the GNU General Public License version 3
- * or the GNU Lesser General Public License version 3, both as
- * published by the Free Software Foundation.
+ * The SPI lines are connected as follows:
+ *    SCK  - Arduino 13 (ATMEGA PORTB5)
+ *    MOSI - Arduino 11 (ATMEGA PORTB3)
+ *    CE   - Arduino 10 (ATMEGA PORTB2)
+ *    DC   - Arduino 9 (ATMEGA PORTB1)
+ *
+ * This file is free software; you can redistribute it and/or modify it under
+ * the terms of either the GNU General Public License version 3 or the GNU
+ * Lesser General Public License version 3, both as published by the Free
+ * Software Foundation.
  */ 
 
 #include <avr/io.h>
 #include <stdlib.h>
 #include "i2c/i2c.h"
+#include "spi/spi.h"
 #include "adxl345/adxl345.h"
 #include "hmc5883/hmc5883.h"
 #include "itg3205/itg3205.h"
 #include "graphics/graphics.h"
-#include "ssd1306/ssd1306_i2c.h"
+#include "ssd1306/ssd1306_spi.h"
 
 /* These define the string formatting for the itoa() function. */
 #define HEX_FORMAT 16
@@ -45,10 +51,13 @@ int main(void)
 /* temporary storage for 16-bit raw sensor data */
   int16_t sensorXData, sensorYData, sensorZData;
 
-/* initialize and start up the I2C system */
+/* initialize the I2C bus for the sensors */
   i2c_init(400000UL);
 
-/* Initialize and power up the accelerometer.
+/* initialize the SPI bus for the display */
+  spi_init((SPI_SPCR_SPE | SPI_SPCR_DORD_MSB | SPI_SPCR_MSTR | SPI_SPCR_MODE3 | SPI_SPCR_DIV2), SPI_SPSR_SPI2X);
+
+/* Initialize the accelerometer.
    
     power it up
     set data format to full resolution and +/-16g
@@ -63,7 +72,7 @@ int main(void)
   itg3205_setPowerMgmt(ITG3205_PWR_MGMT_RESET|ITG3205_PWR_MGMT_PLLZ);
   itg3205_setSampleRate(ITG3205_FS_SEL|ITG3205_DLPF_20HZ);
 
-/* Initialize and power up the magnetometer.
+/* Initialize the magnetometer.
    
     average = 1, data rate = 15Hz
     gain = 1.3 Ga
@@ -76,8 +85,8 @@ int main(void)
 /* Initialize the display.
    Setup the display in graphics mode.
  */
-  ssd1306_i2c_init();
-  ssd1306_i2c_flip_vertical();
+  ssd1306_spi_init(&PORTB, PORTB2, &PORTB, PORTB1);
+  ssd1306_spi_flip_vertical();
   graphics_init(SSD1306_GRAPHICS_MAX_X, SSD1306_GRAPHICS_MAX_Y);
   graphics_set_text_size(2);
   graphics_set_rotation(GRAPHICS_ROTATION_0);
@@ -93,19 +102,19 @@ int main(void)
   graphics_putStr("Gyr:");
   graphics_set_cursor(0, 47);
   graphics_putStr("Mag:");
-  ssd1306_i2c_graphics_update();
+  ssd1306_spi_graphics_update();
 
 /* Repeatedly read the sensors and send the data to the display. */
   while(1) 
   {
-  /* read the accelerometer, format and display the data */
+  /* Read the accelerometer, format and display the data. */
     adxl345_getAccelData(sensorBuf);
-    sensorXData = (int16_t)sensorBuf[1];
-    sensorXData += (int16_t)sensorBuf[0] << 8;
-    sensorYData = (int16_t)sensorBuf[3];
-    sensorYData += (int16_t)sensorBuf[2] << 8;
-    sensorZData = (int16_t)sensorBuf[5];
-    sensorZData += (int16_t)sensorBuf[4] << 8;
+    sensorXData = (int16_t)sensorBuf[1] << 8;
+    sensorXData += (int16_t)sensorBuf[0];
+    sensorYData = (int16_t)sensorBuf[3] << 8;
+    sensorYData += (int16_t)sensorBuf[2];
+    sensorZData = (int16_t)sensorBuf[5] << 8;
+    sensorZData += (int16_t)sensorBuf[4];
 
     graphics_set_cursor(28, 27);
     itoa(sensorXData, tempStr, HEX_FORMAT);
@@ -119,14 +128,14 @@ int main(void)
     itoa(sensorZData, tempStr, HEX_FORMAT);
     graphics_putStr(tempStr);
 
-  /* read the gyroscope, format and display the data */
+  /* Read the gyroscope, format and display the data. */
     itg3205_getGyroData(sensorBuf);
-    sensorXData = (int16_t)sensorBuf[1];
-    sensorXData += (int16_t)sensorBuf[0] << 8;
-    sensorYData = (int16_t)sensorBuf[3];
-    sensorYData += (int16_t)sensorBuf[2] << 8;
-    sensorZData = (int16_t)sensorBuf[5];
-    sensorZData += (int16_t)sensorBuf[4] << 8;
+    sensorXData = (int16_t)sensorBuf[1] << 8;
+    sensorXData += (int16_t)sensorBuf[0];
+    sensorYData = (int16_t)sensorBuf[3] << 8;
+    sensorYData += (int16_t)sensorBuf[2];
+    sensorZData = (int16_t)sensorBuf[5] << 8;
+    sensorZData += (int16_t)sensorBuf[4];
 
     graphics_set_cursor(28, 37);
     itoa(sensorXData, tempStr, HEX_FORMAT);
@@ -140,14 +149,15 @@ int main(void)
     itoa(sensorZData, tempStr, HEX_FORMAT);
     graphics_putStr(tempStr);
 
-  /* read the magnetometer, format and display the data */
+  /* Read the magnetometer, format and display the data.  The data for each axis
+     is read MSB first. */
     hmc5883_getMagData(sensorBuf);
-    sensorXData = (int16_t)sensorBuf[1];
-    sensorXData += (int16_t)sensorBuf[0] << 8;
-    sensorYData = (int16_t)sensorBuf[3];
-    sensorYData += (int16_t)sensorBuf[2] << 8;
-    sensorZData = (int16_t)sensorBuf[5];
-    sensorZData += (int16_t)sensorBuf[4] << 8;
+    sensorXData = (int16_t)sensorBuf[0] << 8;
+    sensorXData += (int16_t)sensorBuf[1];
+    sensorYData = (int16_t)sensorBuf[2] << 8;
+    sensorYData += (int16_t)sensorBuf[3];
+    sensorZData = (int16_t)sensorBuf[4] << 8;
+    sensorZData += (int16_t)sensorBuf[5];
 
     graphics_set_cursor(28, 47);
     itoa(sensorXData, tempStr, HEX_FORMAT);
@@ -162,7 +172,7 @@ int main(void)
     graphics_putStr(tempStr);
 
   /* everything is written to the graphics RAM, now send it to the display */
-    ssd1306_i2c_graphics_update();
+    ssd1306_spi_graphics_update();
 
   }/* end while(1) */
 
